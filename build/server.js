26714bc4c3608e94cfdc5cef59fc1ca10f487e21
CarlieHamilton <carliemhamilton@gmail.com>
2021-03-06 17:08:29 +1000

    Remove build folder

--- build/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const port = 3000;
-// Define Express Server
-const app = express();
-// Listen to the server!!!
-app.listen(port, () => {
-    console.log("Yay I work");
-});
-//# sourceMappingURL=server.js.map
