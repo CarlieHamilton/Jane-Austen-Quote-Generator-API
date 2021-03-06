d8694691cd98b4b92b6c3054b8c1f40bd08dce42
CarlieHamilton <carliemhamilton@gmail.com>
2021-03-06 19:18:33 +1000

    random book quote IT WORKS

--- src/utilities/quotesUtils.ts
+++ src/utilities/quotesUtils.ts
@@ -2,13 +2,17 @@ import { Quote } from '../models/quote.model';
 import * as data from '../data/quotes.json';
 
 export const returnRandomQuote = () => {
-    const quote = randomQuote('Pride and Prejudice');
+    const quote = randomQuote(randomBook());
     return quote;
 }
 
-// function that returns a random bookTitle
+// function that returns a random book title
+const randomBook = () => {
+    const bookTitles = Object.keys(data);
+    return bookTitles[Math.floor(Math.random() * bookTitles.length)]
+}
 
-// function that returns a random quote from that book
+// function that returns a random quote from a specified book
 const randomQuote = (bookTitle: string) => {
     const quotes: string[] = data[bookTitle];
     return quotes[Math.floor(Math.random() * quotes.length)];
