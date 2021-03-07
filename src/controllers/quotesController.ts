b93aebda2d362652f2a7e74432b0b8be26e78879
CarlieHamilton <carliemhamilton@gmail.com>
2021-03-07 19:32:11 +1000

    Added some types
    So I can lay claim that I have actually typescripted

--- src/controllers/quotesController.ts
+++ src/controllers/quotesController.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import { Quote } from '../models/quote.model';
 import {
     returnRandomQuote, returnRandomQuoteFromBook
 } from '../utilities/quotesUtils';
 
 export const getRandomQuote = (request: Request, response: Response) => {
-    const quote = returnRandomQuote();
+    const quote: Quote = returnRandomQuote();
     if (quote != null) {
         response.status(200).json({randomQuote: quote});
     } else {
@@ -14,7 +15,7 @@ export const getRandomQuote = (request: Request, response: Response) => {
 
 export const getRandomQuoteFromBook = (request: Request, response: Response) => {
     const bookTitle = request.body.title;
-    const quote = returnRandomQuoteFromBook(bookTitle);
+    const quote: Quote = returnRandomQuoteFromBook(bookTitle);
     if (quote != null) {
         response.status(200).json({randomQuote: quote});
     } else {
--- src/utilities/quotesUtils.ts
+++ src/utilities/quotesUtils.ts
@@ -2,23 +2,23 @@ import { Quote } from '../models/quote.model';
 import * as data from '../data/quotes.json';
 
 export const returnRandomQuote = () => {
-    const quote = randomQuote(randomBook());
+    const quote: Quote = randomQuote(randomBook());
     return quote;
 }
 
 export const returnRandomQuoteFromBook = (bookTitle: string) => {
-    const quote = randomQuote(bookTitle);
+    const quote: Quote = randomQuote(bookTitle);
     return quote
 }
 
 // function that returns a random book title
 const randomBook = () => {
-    const bookTitles = Object.keys(data);
+    const bookTitles: string[] = Object.keys(data);
     return bookTitles[Math.floor(Math.random() * bookTitles.length)]
 }
 
 // function that returns a random quote from a specified book
 const randomQuote = (bookTitle: string) => {
-    const quotes: string[] = data[bookTitle];
+    const quotes: Quote[] = data[bookTitle];
     return quotes[Math.floor(Math.random() * quotes.length)];
 }
